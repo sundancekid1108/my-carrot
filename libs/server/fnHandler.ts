afe0a3b8536104303ead5da07604609f97124411
sundancekid1108 <sundancekid1108@gmail.com>
2022-11-25 00:44:18 +0900

    REST API 테스트
    REST API 테스트

--- libs/server/fnHandler.ts
+++ libs/server/fnHandler.ts
@@ -1,10 +1,17 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
+export interface ResponseType {
+	ok: boolean;
+	[key: string]: any;
+}
+
 export default function fnHandler(
 	method: "GET" | "POST" | "DELETE",
 	fn: (req: NextApiRequest, res: NextApiResponse) => void
 ) {
 	return async function (req: NextApiRequest, res: NextApiResponse) {
+		// console.log("fnHandler req", req.method);
+		// console.log(method);
 		if (req.method !== method) {
 			return res.status(405).end();
 		}
